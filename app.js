20bc9dbbb4a396f8b990b937dff29447777cba7c
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-06-26 21:13:58 +0100

    Added app.js and popup

---
+++ app.js
@@ -0,0 +1,8 @@
+let loyalty = document.querySelector("#loyalty");
+loyalty.addEventListener("click", function () {
+  const popWin = window.open(
+    "popup.html",
+    "Popup",
+    "top=150,left=200,width=350,height=450"
+  );
+});

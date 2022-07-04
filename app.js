dfd68eedffe05469cc6dbe506a27f4e5913b1618
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-07-04 20:28:54 +0100

    updated styling and JS on info page

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ function toggleSwitch() {
   let element = document.body;
   element.classList.toggle("pink");
 }
+
+let uname = document.querySelector("#contact-text-input");
+let email = document.querySelector("#contact-email-input");
+let message = document.querySelector("#msg");
+function submitForm() {
+  if (uname.value === "") {
+    alert(`Hey, fill me out first!`);
+  } else {
+    uname = uname.value;
+    email = email.value;
+    message = message.value;
+    alert(
+      `Thank you ${uname}, for your message: ${message}. We know we can reach you at ${email}! `
+    );
+  }
+}
